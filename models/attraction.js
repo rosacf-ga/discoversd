868a2e54a933c61a00820bd27f7b3edc636aeab9
rosacf-ga <rc.floress@yahoo.com>
2022-05-21 13:34:57 -0700

    Create attraction and comment model

---
+++ models/attraction.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const commentSchema = new Schema({
+  username: String,
+  userId: { type: mongoose.Schema.Types.ObjectId },
+  content: {type: String, required: true}
+}, {
+  timestamps: true
+})
+
+//an attraction has many comments, a comment belongs to an attraction
+const attractionSchema = new Schema({
+  user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+  attractionName: {type: String, required: true},
+  address: {type: String, required: true}, 
+  website: String,
+  description: {type: String, required: true}, 
+  comments: [commentSchema]
+}, {
+  timestamps: true
+})
+
+module.export = mongoose.model('Attraction', attractionSchema);
