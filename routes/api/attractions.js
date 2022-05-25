8d98f3c49bfa61c47c27f097332543c1da7be67a
rosacf-ga <rc.floress@yahoo.com>
2022-05-25 16:53:53 -0700

    Create isLoggedIn function

--- routes/api/attractions.js
+++ routes/api/attractions.js
@@ -6,11 +6,16 @@ const upload = multer(); // <- handles multipart/formdata requests(photos)
 
 // /*---------- Public Routes ----------*/
 router.post('/', upload.single('photo'), attractionsCtrl.create);
-router.get('/', attractionsCtrl.index)
-router.get('/:id', attractionsCtrl.show);
+router.get('/', isLoggedIn, attractionsCtrl.index)
+router.get('/:id', isLoggedIn, attractionsCtrl.show);
 router.delete('/:id', attractionsCtrl.delete);
 
 
 /*---------- Protected Routes ----------*/
+
+function isLoggedIn(req, res, next){
+  if (req.user) next();
+  res.status(401).json({data: 'Not authorized. Please log in.'})
+}
 
 module.exports = router;
