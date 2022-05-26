558ef46579807a52d495e8e50dd4ebf2a3f11f73
rosacf-ga <rc.floress@yahoo.com>
2022-05-25 17:03:27 -0700

    Remove isLoggedIn due to causing errors

--- routes/api/attractions.js
+++ routes/api/attractions.js
@@ -6,16 +6,11 @@ const upload = multer(); // <- handles multipart/formdata requests(photos)
 
 // /*---------- Public Routes ----------*/
 router.post('/', upload.single('photo'), attractionsCtrl.create);
-router.get('/', isLoggedIn, attractionsCtrl.index)
-router.get('/:id', isLoggedIn, attractionsCtrl.show);
+router.get('/', attractionsCtrl.index)
+router.get('/:id', attractionsCtrl.show);
 router.delete('/:id', attractionsCtrl.delete);
 
 
 /*---------- Protected Routes ----------*/
-
-function isLoggedIn(req, res, next){
-  if (req.user) next();
-  res.status(401).json({data: 'Not authorized. Please log in.'})
-}
 
 module.exports = router;
