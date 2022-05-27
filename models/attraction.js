337441262ab3831772eec0b157b1456f8699761f
rosacf-ga <rc.floress@yahoo.com>
2022-05-26 21:12:22 -0700

    Remove commented out code

--- models/attraction.js
+++ models/attraction.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-//an attraction has many comments, a comment belongs to an attraction
 const attractionSchema = new Schema({
   user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
   attractionName: String,
